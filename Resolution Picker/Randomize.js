eaaf3fde64da07a20d2c12b3f9fe341090287f97
Milan Donhowe <milanaugust@gmail.com>
2018-04-23 20:07:30 -0700

    Delete Randomize.js

--- Resolution Picker/Randomize.js
+++
@@ -1,8 +0,0 @@
-// Randomize resolution
-
-
-document.getElementById("btn").addEventListener("click", function() {
-    //randomly select resolution
-    document.getElementById('Change').textContent = resolved.resolutions[Math.floor(Math.random() * resolved.resolutions.length)];
-
-});
